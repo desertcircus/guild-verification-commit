cefe3b258ef03faae4adad4dc750dcb906d58350
Burak <150270944+desertcircus@users.noreply.github.com>
2025-11-17 17:35:10 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-17T14:35:04.032Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-17T14:35:09.874Z
+Commit: Add export functionality for reports
